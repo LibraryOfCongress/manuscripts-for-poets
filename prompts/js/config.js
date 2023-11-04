791d9adb769aea15f51d417429754b951752e70b
beefoo <brian@youaremyjoy.org>
2023-11-04 14:49:16 -0400

    build timeline html

--- prompts/js/config.js
+++ prompts/js/config.js
@@ -2,15 +2,59 @@ const config = {
   'mary-church-terrell': {
     timeline: [
       {
-        start: 1863, end: 1877, title: 'Reconstruction', description: '',
+        year: 1863, title: 'Born, Memphis, Tenn.', description: '',
       }, {
-        start: 1877, end: 1950, title: 'Nadir Period', description: '',
+        start: 1863, end: 1877, title: 'Reconstruction Period', description: '', type: 'era',
       }, {
-        start: 1880, end: 1949, title: 'Jim Crow Era', description: '',
+        start: 1877, end: 1950, title: 'Nadir Period', description: '', type: 'era',
       }, {
-        start: 1929, end: 1939, title: 'The Great Depression', description: '',
+        start: 1880, end: 1949, title: 'Jim Crow Era', description: '', type: 'era',
       }, {
-        start: 1954, end: 1968, title: 'Civil Rights Movement', description: '',
+        start: 1885, end: 1887, text: 'Taught at Wilberforce University', description: '',
+      }, {
+        start: 1887, end: 1888, text: 'Taught at High School for Colored Youth', description: '',
+      }, {
+        year: 1888, text: 'Attended Oberlin College', description: '',
+      }, {
+        start: 1888, end: 1890, text: 'Studied and traveled in France, Germany, Switzerland, and Italy', description: '',
+      }, {
+        start: 1890, end: 1891, text: 'Resumed teaching, High School for Colored Youth', description: '',
+      }, {
+        year: 1891, text: 'Married Robert H. Terrell', description: '',
+      }, {
+        start: 1895, end: 1901, text: 'Appointed to District of Columbia School Board', description: '',
+      }, {
+        year: 1896, text: 'Organized and became first president of the National Association of Colored Women', description: '',
+      }, {
+        start: 1898, end: 1920, text: 'Active in woman\'s suffrage movement', description: '',
+      }, {
+        year: 1904, text: 'Addressed International Congress of Women, Berlin, Germany', description: '',
+      }, {
+        start: 1906, end: 1911, text: 'Reappointed to District of Columbia School Board', description: '',
+      }, {
+        year: 1909, text: 'Charter member, National Association for the Advancement of Colored People', description: '',
+      }, {
+        start: 1918, end: 1919, text: 'Served in War Camp Community Service', description: '',
+      }, {
+        year: 1919, text: 'Addressed Women\'s International League for Peace and Freedom, Zurich, Switzerland', description: '',
+      }, {
+        year: 1920, text: 'Appointed supervisor, Committee for Eastern District Work among Colored Women, Republican National Committee', description: '',
+      }, {
+        start: 1929, end: 1939, title: 'The Great Depression', description: '', type: 'era',
+      }, {
+        start: 1929, end: 1930, text: 'Campaigned for Ruth Hanna McCormick, Republican candidate for U.S. Senate from Illinois', description: '',
+      }, {
+        year: 1932, text: 'Served as adviser to the Republican National Committee, Herbert Hoover presidential campaign', description: '',
+      }, {
+        year: 1937, text: 'Represented American Black women at World Fellowship of Faiths, London, England', description: '',
+      }, {
+        year: 1940, text: 'Published autobiography, A Colored Woman in a White World. Washington, D.C.: Ransdell', description: '',
+      }, {
+        year: 1949, text: 'Elected chairman, Coordinating Committee for the Enforcement of the D.C. Anti-Discrimination Laws', description: '',
+      }, {
+        year: 1954, title: 'Died, Annapolis, Md.',
+      }, {
+        start: 1954, end: 1968, title: 'Civil Rights Movement', description: '', type: 'era',
       },
     ],
   },
