bf60d613ad016ea54ebb795b79205cb7edfc27b8
beefoo <brian@youaremyjoy.org>
2023-11-04 12:02:24 -0400

    create a config file

---
+++ prompts/js/config.js
@@ -0,0 +1,17 @@
+const config = {
+  'mary-church-terrell': {
+    timeline: [
+      {
+        start: 1863, end: 1877, title: 'Reconstruction', description: '',
+      }, {
+        start: 1877, end: 1950, title: 'Nadir Period', description: '',
+      }, {
+        start: 1880, end: 1949, title: 'Jim Crow Era', description: '',
+      }, {
+        start: 1929, end: 1939, title: 'The Great Depression', description: '',
+      }, {
+        start: 1954, end: 1968, title: 'Civil Rights Movement', description: '',
+      },
+    ],
+  },
+};
