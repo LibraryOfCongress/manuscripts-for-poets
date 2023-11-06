2425d236de6a0f217f48055b20a32650122021fc
beefoo <brian@youaremyjoy.org>
2023-11-06 17:04:20 -0500

    add artist statement, update about, update timeline

--- public/prompts/js/config.js
+++ public/prompts/js/config.js
@@ -6,9 +6,9 @@ const config = {
       }, {
         start: 1863, end: 1877, text: 'Reconstruction Period', description: '', type: 'era',
       }, {
-        start: 1877, end: 1950, text: 'Nadir Period', description: '', type: 'era',
+        start: 1877, end: 1950, text: 'Nadir Period', description: 'A period of time where ceaseless extrajudicial violence (lynchings) were enacted in the South. Considered one of the most violent time periods for Black Americans in U.S History after slavery. Ida B. Wells “A Red Record”', type: 'era',
       }, {
-        start: 1880, end: 1949, text: 'Jim Crow Era', description: '', type: 'era',
+        start: 1880, end: 1949, text: 'Jim Crow Era', description: 'Due to violence surrounding voting, Black male voter registration drops to just 6%', type: 'era',
       }, {
         start: 1885, end: 1887, text: 'Taught at Wilberforce University', description: '',
       }, {
@@ -34,7 +34,11 @@ const config = {
       }, {
         year: 1909, text: 'Charter member, National Association for the Advancement of Colored People', description: '',
       }, {
+        start: 1914, end: 1918, text: 'World War I', description: 'World War I created a transformation for African Americans from the “old” to the “new.” Thousands moved from the rural South to the industrial urban North, pursuing a new vision of social and economic opportunity. During the war black troops fought abroad “to keep the world safe for democracy.” They returned home determined to achieve a fuller participation in American society. The philosophy of the civil rights movement shifted from the “accommodationist” approach of Booker T. Washington to the militant advocacy of W.E.B. Du Bois. These forces converged to help create the “New Negro Movement” of the 1920s, which promoted a renewed sense of racial pride, cultural self-expression, economic independence, and progressive politics.',
+      }, {
         start: 1918, end: 1919, text: 'Served in War Camp Community Service', description: '',
+      }, {
+        year: 1918, text: 'Anti-lynching bill first introduced', description: 'Introduced to United States House of Representatives and shot down in U.S. Senate by fillibuster',
       }, {
         year: 1919, text: 'Addressed Women\'s International League for Peace and Freedom, Zurich, Switzerland', description: '',
       }, {
@@ -52,9 +56,9 @@ const config = {
       }, {
         year: 1949, text: 'Elected chairman, Coordinating Committee for the Enforcement of the D.C. Anti-Discrimination Laws', description: '',
       }, {
-        year: 1954, text: 'Died, Annapolis, Md.',
+        year: 1954, text: 'Brown v. Board of Education', description: 'The end of state sanctioned desegregation in schools and the beginning of the Civil Rights era in the United States.', type: 'era',
       }, {
-        start: 1954, end: 1968, text: 'Civil Rights Movement', description: '', type: 'era',
+        year: 1954, text: 'Died, Annapolis, Md.',
       },
     ],
   },
