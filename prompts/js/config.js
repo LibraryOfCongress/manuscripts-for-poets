107f481e20fc4dda51d438d3ff7f7bff66240002
beefoo <brian@youaremyjoy.org>
2023-11-08 10:32:47 -0500

    put Terrell name in timeline, make colors meaningful

--- prompts/js/config.js
+++ prompts/js/config.js
@@ -2,63 +2,63 @@ const config = {
   'mary-church-terrell': {
     timeline: [
       {
-        year: 1863, text: 'Born, Memphis, Tenn.', description: '',
-      }, {
         start: 1863, end: 1877, text: 'Reconstruction Period', description: '', type: 'era',
+      }, {
+        year: 1863, text: 'Terrell Born, Memphis, Tenn.', description: '',
       }, {
         start: 1877, end: 1950, text: 'Nadir Period', description: 'A period of time where ceaseless extrajudicial violence (lynchings) were enacted in the South. Considered one of the most violent time periods for Black Americans in U.S History after slavery. Ida B. Wells “A Red Record”', type: 'era',
       }, {
         start: 1880, end: 1949, text: 'Jim Crow Era', description: 'Due to violence surrounding voting, Black male voter registration drops to just 6%', type: 'era',
       }, {
-        start: 1885, end: 1887, text: 'Taught at Wilberforce University', description: '',
+        start: 1885, end: 1887, text: 'Terrell teaches at Wilberforce University', description: '',
       }, {
-        start: 1887, end: 1888, text: 'Taught at High School for Colored Youth', description: '',
+        start: 1887, end: 1888, text: 'Terrell teaches at High School for Colored Youth', description: '',
       }, {
-        year: 1888, text: 'Attended Oberlin College', description: '',
+        year: 1888, text: 'Terrell attends Oberlin College', description: '',
       }, {
-        start: 1888, end: 1890, text: 'Studied and traveled in France, Germany, Switzerland, and Italy', description: '',
+        start: 1888, end: 1890, text: 'Terrell studies and travels in France, Germany, Switzerland, and Italy', description: '',
       }, {
-        start: 1890, end: 1891, text: 'Resumed teaching, High School for Colored Youth', description: '',
+        start: 1890, end: 1891, text: 'Terrell resumes teaching, High School for Colored Youth', description: '',
       }, {
-        year: 1891, text: 'Married Robert H. Terrell', description: '',
+        year: 1891, text: 'Terrell marries Robert H. Terrell', description: '',
       }, {
-        start: 1895, end: 1901, text: 'Appointed to District of Columbia School Board', description: '',
+        start: 1895, end: 1901, text: 'Terrell appointed to District of Columbia School Board', description: '',
       }, {
-        year: 1896, text: 'Organized and became first president of the National Association of Colored Women', description: '',
+        year: 1896, text: 'Terrell organizes and becomes first president of the National Association of Colored Women', description: '',
       }, {
-        start: 1898, end: 1920, text: 'Active in woman\'s suffrage movement', description: '',
+        start: 1898, end: 1920, text: 'Terrell active in woman\'s suffrage movement', description: '',
       }, {
-        year: 1904, text: 'Addressed International Congress of Women, Berlin, Germany', description: '',
+        year: 1904, text: 'Terrell addresses International Congress of Women, Berlin, Germany', description: '',
       }, {
-        start: 1906, end: 1911, text: 'Reappointed to District of Columbia School Board', description: '',
+        start: 1906, end: 1911, text: 'Terrell reappointed to District of Columbia School Board', description: '',
       }, {
-        year: 1909, text: 'Charter member, National Association for the Advancement of Colored People', description: '',
+        year: 1909, text: 'Terrell becomes charter member of National Association for the Advancement of Colored People', description: '',
       }, {
         start: 1914, end: 1918, text: 'World War I', description: 'World War I created a transformation for African Americans from the “old” to the “new.” Thousands moved from the rural South to the industrial urban North, pursuing a new vision of social and economic opportunity. During the war black troops fought abroad “to keep the world safe for democracy.” They returned home determined to achieve a fuller participation in American society. The philosophy of the civil rights movement shifted from the “accommodationist” approach of Booker T. Washington to the militant advocacy of W.E.B. Du Bois. These forces converged to help create the “New Negro Movement” of the 1920s, which promoted a renewed sense of racial pride, cultural self-expression, economic independence, and progressive politics.',
       }, {
-        start: 1918, end: 1919, text: 'Served in War Camp Community Service', description: '',
+        start: 1918, end: 1919, text: 'Terrell serves in War Camp Community Service', description: '',
       }, {
-        year: 1918, text: 'Anti-lynching bill first introduced', description: 'Introduced to United States House of Representatives and shot down in U.S. Senate by fillibuster',
+        year: 1918, text: 'Anti-lynching bill first introduced', description: 'Introduced to United States House of Representatives and shot down in U.S. Senate by fillibuster', type: 'bill',
       }, {
-        year: 1919, text: 'Addressed Women\'s International League for Peace and Freedom, Zurich, Switzerland', description: '',
+        year: 1919, text: 'Terrell addresses Women\'s International League for Peace and Freedom, Zurich, Switzerland', description: '',
       }, {
-        year: 1920, text: 'Appointed supervisor, Committee for Eastern District Work among Colored Women, Republican National Committee', description: '',
+        year: 1920, text: 'Terrell appointed supervisor, Committee for Eastern District Work among Colored Women, Republican National Committee', description: '',
       }, {
         start: 1929, end: 1939, text: 'The Great Depression', description: '', type: 'era',
       }, {
-        start: 1929, end: 1930, text: 'Campaigned for Ruth Hanna McCormick, Republican candidate for U.S. Senate from Illinois', description: '',
+        start: 1929, end: 1930, text: 'Terrell campaigns for Ruth Hanna McCormick, Republican candidate for U.S. Senate from Illinois', description: '',
       }, {
-        year: 1932, text: 'Served as adviser to the Republican National Committee, Herbert Hoover presidential campaign', description: '',
+        year: 1932, text: 'Terrell serves as adviser to the Republican National Committee, Herbert Hoover presidential campaign', description: '',
       }, {
-        year: 1937, text: 'Represented American Black women at World Fellowship of Faiths, London, England', description: '',
+        year: 1937, text: 'Terrell represents American Black women at World Fellowship of Faiths, London, England', description: '',
       }, {
-        year: 1940, text: 'Published autobiography, A Colored Woman in a White World. Washington, D.C.: Ransdell', description: '',
+        year: 1940, text: 'Terrell publishes autobiography, A Colored Woman in a White World. Washington, D.C.: Ransdell', description: '',
       }, {
-        year: 1949, text: 'Elected chairman, Coordinating Committee for the Enforcement of the D.C. Anti-Discrimination Laws', description: '',
+        year: 1949, text: 'Terrell elected chairman, Coordinating Committee for the Enforcement of the D.C. Anti-Discrimination Laws', description: '',
       }, {
-        year: 1954, text: 'Brown v. Board of Education', description: 'The end of state sanctioned desegregation in schools and the beginning of the Civil Rights era in the United States.', type: 'era',
+        year: 1954, text: 'Brown v. Board of Education', description: 'The end of state sanctioned desegregation in schools and the beginning of the Civil Rights era in the United States.', type: 'bill',
       }, {
-        year: 1954, text: 'Died, Annapolis, Md.',
+        year: 1954, text: 'Terrell Dies, Annapolis, Md.',
       },
     ],
   },
